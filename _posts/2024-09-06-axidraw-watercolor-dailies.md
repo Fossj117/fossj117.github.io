022af6b3392bc0c85af74e1560399758c0c32594
fossj117 <fossj117@gmail.com>
2024-09-10 11:06:51 -0400

    watercolor dailies midway

---
+++ _posts/2024-09-06-axidraw-watercolor-dailies.md
@@ -0,0 +1,100 @@
+---
+layout: post
+title: "Pen Plotter & Watercolor Dailies"
+date: 2024-09-06
+latex: true
+mathjax: true
+comments: true
+tag: ["art"]
+---
+
+## Motivation
+
+Lately, I have been thinking about the value of discipline and focus in creative work. For me, there can be an impulse to jump around when I'm in a "creative" headspace -- I want to work on what's exciting and engaging in the moment, which often involves trying or learning something new. 
+
+While this approach can be motivating and enjoyable, it also has limitations. There are lots of interesting and rewarding projects that can only be completed with sustained focus and commmitment; focus allows for more completely exploring and refining an idea. Further, many projects involve some moments of frustration or annoyance at some point or another -- some tasks that I feel resistant towards completing. It can be difficult to work through these moments of resistance under the excitement-oriented approach. 
+
+Finally, there's an energetic value to discipline and that I want to explore. Discipline and consistency have a grounded, stable, secure feeling that contrasts with the untethered, fluid, uncertain energy of novelty and excitement. 
+
+With that in mind, I am doing some experiments in creative consistency. Here is one experiment. Today is Sunday. Each day this week, I am going to make one generative drawing in a series. There will be seven drawings in the series, and they will all be made using the same (or a similar) process. The process I want to explore is involves combining generative art and the axidraw with water-soluble fountain pen ink and watercolors. 
+
+## Design
+
+I have two sheets of leftover 9x12" watercolor paper; I will divide them into 4x6" rectangles for the drawings. The drawings will have a 1/2" border, so the main drawings will be 3x5". I have a simple concept in mind for the generative drawings. 
+
+Here is the plan: 
+
+1. Split the canvas into an array of squares. 
+2. Create a "fracture" from top to bottom of the canvas. 
+3. Fill squares in different ways in relation to their distance from the fracture. 
+
+I want to pair the machine drawing with some hand water-coloring, so I will go in with that at the end.
+
+## Results 
+
+### Day One
+
+Here is the result for day 1, before and after water colors: 
+
+<div style="display: flex; justify-content: center;">
+  <img src="/figs/2024-09-06-axidraw-watercolor-dailies/day1.jpg" alt="Image 1" style="width: 45%; max-width: 45%" >
+  <img src="/figs/2024-09-06-axidraw-watercolor-dailies/day1wc.jpg" alt="Image 2" style="width: 45%; max-width: 45%">
+</div>
+
+I don't like these results much. The pen drawing is messy, and the watercolor felt unintentional and disconnected. I wasn't really sure what my plan was. But the goal of this experiment is to iterate, trust the process. What can i do different tomorrow? 
+
+I think tomorrow I will try using water-soluble fountain pen ink. That was my original plan, but i wasn't set up for it. I hope it will connect the pen and the watercolor more. I will go lighter on the fill as well, and introduce some more variation in the design. 
+
+I think this piece is about something like the boundary between control and uncertainty. Maybe I'll try putting the colors in a different place. Maybe there should be some white space remaining. Maybe I should invert the positive and negative space. Maybe I should allow for more variation in the fill distributions across the vertical dimension of the piece.  
+
+### Day Two
+
+Here is the result for day 2; I wound up doing two today. I also started naming them.  
+
+<!-- {% raw %}
+<div style="display: flex; justify-content: center; gap: 20px;">
+  <div style="text-align: center; flex-basis: 45%;">
+    <img src="/figs/2024-09-06-axidraw-watercolor-dailies/day2A.jpg" alt="Struggling on the Boundaries" style="width: 100%; height: auto;">
+    <div><em>Struggling on the Boundaries</em></div>
+  </div>
+  <div style="text-align: center; flex-basis: 45%;">
+    <img src="/figs/2024-09-06-axidraw-watercolor-dailies/day2B.jpg" alt="Computers & the World" style="width: 100%; height: auto;">
+    <div><em>Computers & the World</em></div>
+  </div>
+</div>
+{% endraw %} -->
+
+
+<div align="center">
+
+<img src="/figs/2024-09-06-axidraw-watercolor-dailies/day2A.jpg" alt="Struggling on the Boundaries" style="width: 50%;" />
+
+*Struggling on the Boundaries*
+
+<img src="/figs/2024-09-06-axidraw-watercolor-dailies/day2B.jpg" alt="Computers & the World" style="width: 50%;" />
+
+*Computers & the World*
+
+</div>
+
+These are still pretty simple in terms of outputs, but I am starting to find them a bit more interesting. I used the water soluble ink and used a watercolor brush to paint just water on to the canvas before and as the drawing was happening. In general, I am interested in work that emerges from some complex interaction of machine, human, and natural elements like water. 
+
+In this process, the routing of the pen plotting algorithm and the application of volume of the water (and its rate of drying) matter a lot. Something I found interesting is layering, and so i want to think about how to explore that more.
+
+What else? How might I make this more interesting? More colors, more feedback? More complexity in the digital design? Perhaps the application of the water might be even more dynamic and interactive.
+
+### Day Three 
+
+Today's experiment involves some [automatic drawing](https://en.wikipedia.org/wiki/Surrealist_automatism) with watercolor pencils. The result is chaotic and not too appealing for me, but there's something conceptually interesting about the idea of combining human and machine automatism, alongside an injection of natural randomness (water). 
+
+<div align="center">
+
+<img src="/figs/2024-09-06-axidraw-watercolor-dailies/day3A.jpg" alt="Indulgence" style="width: 50%;" />
+
+*Indulgence*
+
+<img src="/figs/2024-09-06-axidraw-watercolor-dailies/day3B.jpeg" alt="Indulgence (process)" style="width: 50%;" />
+
+*Indulgence* (process)
+
+</div>
---
+++ assets/2024-09-06-axidraw-watercolor-dailies/design.py
@@ -0,0 +1,191 @@
+import random
+import svgwrite
+
+def distance_xy(x1, y1, x2, y2):
+    return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+
+class Square: 
+    def __init__(self, x, y, size, fill, dwg):
+        '''
+        X and Y are the coordinates of the square in the grid
+
+        fill can be "Light_Fill", "Heavy_Fill", "No_Fill" or "None"
+        '''
+        self.x = x
+        self.y = y
+        self.size = size
+
+        self.fill = fill
+        #self.fill_weights = fill_weights
+        #self.fill_categories = ['Light_Fill', 'Heavy_Fill', 'No_Fill', 'None']
+
+        # Assert that fillweights must be same length as fillCategories
+        #assert len(self.fill_weights) == len(self.fill_categories)
+
+        self.dwg = dwg
+
+    def chooseFill(self):
+        if self.fill: 
+            return self.fill
+        else: 
+            return 'None'
+
+    def drawFilledAt(self,x_pix,y_pix, num_lines): 
+        self.dwg.add(
+                self.dwg.rect(
+                    (x_pix, y_pix), 
+                    (self.size, self.size), 
+                    fill='none', stroke='black')
+                )
+        # create lines at regular intervals to fill the shape
+       # num_lines = random.choice([2,3,4,5,6])
+        orientation = random.choice(['horizontal', 'vertical'])
+
+        if orientation == 'horizontal':
+            for i in range(num_lines):
+                self.dwg.add(
+                    self.dwg.line(
+                        (x_pix + i * self.size/num_lines, y_pix),
+                        (x_pix + i * self.size/num_lines, y_pix + self.size),
+                        stroke='black'
+                    )
+                )
+        else:
+            for i in range(num_lines):
+                self.dwg.add(
+                    self.dwg.line(
+                        (x_pix, y_pix + i * self.size/num_lines),
+                        (x_pix + self.size, y_pix + i * self.size/num_lines),
+                        stroke='black'
+                    )
+                )
+
+    def drawNoFillAt(self,x_pix,y_pix):
+        self.dwg.add(
+                self.dwg.rect(
+                    (x_pix, y_pix), 
+                    (self.size, self.size), 
+                    fill='none', stroke='black')
+        )
+
+    def draw(self):
+        # Draw at the location defined by the x and y coordinates
+        # So it's at location x*size, y*size
+        x_pix = self.x * self.size 
+        y_pix = self.y * self.size
+
+        self.fill = self.chooseFill()
+
+        if self.fill == 'Light_Fill':
+            self.drawFilledAt(x_pix, y_pix, 3)
+        elif self.fill == 'Heavy_Fill':
+            self.drawFilledAt(x_pix, y_pix, 6)
+        elif self.fill == 'No_Fill':
+            self.drawNoFillAt(x_pix, y_pix)
+
+class Fracture: 
+    def __init__(self, start_x, num_rows): 
+        self.start_x = start_x
+        self.num_rows = num_rows
+    
+    def create(self): 
+        curr_row = 0
+        curr_x = self.start_x
+        locations = []
+
+        while curr_row < self.num_rows:
+            locations.append(curr_x)
+            direction = random.choice([-1, 1])
+            curr_x = curr_x + direction
+            curr_row += 1
+        
+        return locations
+
+class Focal: 
+    def __init__(self, max_x, max_y): 
+        self.max_x = max_x
+        self.max_y = max_y
+
+    def max_distance_from_focal(self, x, y):
+
+        distances = [
+            distance_xy(x, y, 0, 0),
+            distance_xy(x, y, 0, self.max_y),
+            distance_xy(x, y, self.max_x, 0),
+            distance_xy(x, y, self.max_x, self.max_y)
+        ]
+
+        return max(distances)
+    
+    def create(self): 
+        x = random.randint(0, self.max_x)
+        y = random.randint(0, self.max_y)
+        return (x, y)
+
+def main(): 
+
+    X_size_pixels = 300
+    Y_size_pixels = 500
+    square_size_pixels = 10
+
+    num_x_pixels = X_size_pixels // square_size_pixels
+    num_y_pixels = Y_size_pixels // square_size_pixels
+
+    fracture = Fracture(num_x_pixels//2, num_y_pixels)
+    fracture_locations = fracture.create()
+
+    focal = Focal(num_x_pixels, num_y_pixels)
+    focal_location = focal.create()
+
+    dwg = svgwrite.Drawing('output.svg', size=(X_size_pixels, Y_size_pixels))
+
+    # Create a list of squares
+    squares = []
+    for y in range(num_y_pixels):
+        for x in range(num_x_pixels):
+
+            # Fracture distances
+            curr_fracture_location = fracture_locations[y]
+            distance_from_fracture = abs(curr_fracture_location - x)
+            max_distance_from_fracture = max(curr_fracture_location, abs(num_x_pixels - curr_fracture_location))
+            normalized_distance_from_fracture = distance_from_fracture / max_distance_from_fracture # 0 to 1
+
+            # Focal distances
+            focal_x, focal_y = focal_location
+            distance_from_focal = distance_xy(x, y, focal_x, focal_y)
+            max_distance_from_focal = focal.max_distance_from_focal(x, y)
+            normalized_distance_from_focal = distance_from_focal / max_distance_from_focal
+
+            # Choose render style based on distance from fracture
+            # Can be "Fill", "No Fill" or "None" with probability based on distance from fracture
+
+            fill = 'None'
+            if random.random() < normalized_distance_from_fracture*.5:
+                
+                if random.random() < normalized_distance_from_fracture*.5:
+                    fill = 'Light_Fill'
+                else: 
+                    fill = 'Heavy_Fill'
+
+            elif random.random() > normalized_distance_from_fracture*1.1:
+                fill = 'No_Fill'
+
+            if distance_from_fracture < 3 and random.random() > 0.1: 
+                fill = 'None'
+
+            if random.random() > 3*normalized_distance_from_focal: 
+                fill = 'None'
+
+            squares.append(Square(x, y, square_size_pixels, fill, dwg))
+
+    # Draw all the squares
+    for square in squares:
+        square.draw()
+    
+    dwg.save()
+
+
+if __name__ == '__main__':
+    main()
+
+
